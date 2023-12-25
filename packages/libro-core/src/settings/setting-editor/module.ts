23ade6967c90dc8fc653158386ec277157e4cd19
sunshinesmilelk <1176136681@qq.com>
2023-12-25 10:35:02 +0800

    fix(core): unregister useless code

--- packages/libro-core/src/settings/setting-editor/module.ts
+++ packages/libro-core/src/settings/setting-editor/module.ts
@@ -3,15 +3,15 @@ import { ManaModule, ConfigurationModule } from '@difizen/mana-app';
 import { ConfigurationPanelView } from './configuration-panel-view.js';
 import { DefaultConfigurationRenderContribution } from './configuration-render-contribution.js';
 import { SettingEditorView } from './setting-editor-view.js';
-import { SettingTreeService } from './setting-tree-service.js';
-import { SettingTreeView } from './setting-tree-view.js';
+// import { SettingTreeService } from './setting-tree-service.js';
+// import { SettingTreeView } from './setting-tree-view.js';
 
 export const SettingEditorModule = ManaModule.create()
   .register(
     SettingEditorView,
     DefaultConfigurationRenderContribution,
     ConfigurationPanelView,
-    SettingTreeView,
-    SettingTreeService,
+    // SettingTreeView,
+    // SettingTreeService,
   )
   .dependOn(ConfigurationModule);
