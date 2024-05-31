2543ab8b3170c33dab071f6bc6d0228c98238574
sunshinesmilelk <1176136681@qq.com>
2024-05-31 12:01:25 +0800

    fix: export KernelAndTerminalPanelView

--- packages/libro-lab/src/index.ts
+++ packages/libro-lab/src/index.ts
@@ -7,6 +7,7 @@ export * from './command/index.js';
 export * from './toc/index.js';
 export * from './welcome/index.js';
 export * from './kernel-manager/index.js';
+export * from './kernel-and-terminal-panel/index.js';
 export * from './common/index.js';
 export * from './image-viewer/index.js';
 export * from './editor-viewer/index.js';
