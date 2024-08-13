f292e0b7288e32e2ef563551eaa49d42621d2458
zhanba <c5e1856@gmail.com>
2024-08-13 19:21:39 +0800

    fix: add missing kernelspec dep

--- packages/libro-kernel/src/server/server-module.ts
+++ packages/libro-kernel/src/server/server-module.ts
@@ -1,9 +1,10 @@
 import { ManaModule } from '@difizen/mana-app';
 
+import { LibroKernelSpecModule } from '../index.js';
+
 import { ServerConnection } from './server-connection.js';
 import { ServerManager } from './server-manager.js';
 
-export const LibroServerModule = ManaModule.create().register(
-  ServerConnection,
-  ServerManager,
-);
+export const LibroServerModule = ManaModule.create()
+  .register(ServerConnection, ServerManager)
+  .dependOn(LibroKernelSpecModule);
