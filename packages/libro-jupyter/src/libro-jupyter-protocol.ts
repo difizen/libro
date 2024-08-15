3efffd5dd3998e3106dca525cd5fb66ed593231d
sunshinesmilelk <1176136681@qq.com>
2024-08-15 15:35:46 +0800

    fix(jupyter): fix the condition when execution btn show tip

--- packages/libro-jupyter/src/libro-jupyter-protocol.ts
+++ packages/libro-jupyter/src/libro-jupyter-protocol.ts
@@ -1,3 +1,4 @@
+import { blue, gold, green, red } from '@ant-design/colors';
 import type {
   ICellMetadata,
   ICodeCellMetadata,
@@ -7,6 +8,7 @@ import type {
 import { ExecutableCellModel } from '@difizen/libro-core';
 import type { IContentsModel } from '@difizen/libro-kernel';
 import type { Event as ManaEvent, Emitter } from '@difizen/mana-app';
+import { l10n } from '@difizen/mana-l10n';
 
 export interface ExecutionMeta extends PartialJSONObject {
   'shell.execute_reply.started': string; // Kernel 开始执行任务时间在 metadata 中的 key
@@ -59,3 +61,95 @@ export interface ServerLaunchManager {
 }
 
 export const libroArgsMimetype = 'application/vnd.libro.args+json';
+
+export interface ServerStatus {
+  category: string;
+  color: string;
+  text: string;
+  text_zh: string;
+}
+
+export const statusToColor = {
+  canRunImmediate: green[5],
+  canRun: blue[5],
+  blocking: gold[5],
+  error: red[4],
+};
+
+export const jupyterServiceStatus: Record<string, ServerStatus> = {
+  loading: {
+    category: 'JupyterService',
+    color: statusToColor.blocking,
+    text: 'loading',
+    text_zh: l10n.t('加载中'),
+  },
+  failed: {
+    category: 'JupyterService',
+    color: statusToColor.error,
+    text: 'failed',
+    text_zh: l10n.t('加载失败'),
+  },
+  loaded: {
+    category: 'JupyterService',
+    color: statusToColor.canRunImmediate,
+    text: 'loaded',
+    text_zh: l10n.t('加载完成'),
+  },
+};
+
+export const kernelStatus: Record<string, ServerStatus> = {
+  connecting: {
+    category: 'Kernel',
+    color: statusToColor.blocking,
+    text: 'connecting',
+    text_zh: l10n.t('正在连接'),
+  },
+  unknown: {
+    category: 'Kernel',
+    color: statusToColor.blocking,
+    text: 'unknown',
+    text_zh: l10n.t('未知'),
+  },
+  starting: {
+    category: 'Kernel',
+    color: statusToColor.blocking,
+    text: 'starting',
+    text_zh: l10n.t('启动中'),
+  },
+  idle: {
+    category: 'Kernel',
+    color: statusToColor.canRunImmediate,
+    text: 'idle',
+    text_zh: l10n.t('空闲'),
+  },
+  busy: {
+    category: 'Kernel',
+    color: statusToColor.canRun,
+    text: 'busy',
+    text_zh: l10n.t('忙碌'),
+  },
+  terminating: {
+    category: 'Kernel',
+    color: statusToColor.blocking,
+    text: 'terminating',
+    text_zh: l10n.t('终止中'),
+  },
+  restarting: {
+    category: 'Kernel',
+    color: statusToColor.blocking,
+    text: 'restarting',
+    text_zh: l10n.t('重启中'),
+  },
+  autorestarting: {
+    category: 'Kernel',
+    color: statusToColor.blocking,
+    text: 'autorestarting',
+    text_zh: l10n.t('自动重启中'),
+  },
+  dead: {
+    category: 'Kernel',
+    color: statusToColor.error,
+    text: 'dead',
+    text_zh: l10n.t('死亡'),
+  },
+};
--- packages/libro-jupyter/src/toolbar/kernel-status-and-selector.tsx
+++ packages/libro-jupyter/src/toolbar/kernel-status-and-selector.tsx
@@ -1,4 +1,3 @@
-import { red, green, gold, blue } from '@ant-design/colors';
 import { LoadingOutlined, StopOutlined } from '@ant-design/icons';
 import type { LibroView } from '@difizen/libro-core';
 import { ServerManager } from '@difizen/libro-kernel';
@@ -6,132 +5,13 @@ import { useInject, ViewInstance } from '@difizen/mana-app';
 import { l10n } from '@difizen/mana-l10n';
 import { Badge } from 'antd';
 
-import { LibroJupyterModel } from '../libro-jupyter-model.js';
-
-import { KernelSelector } from './kernel-selector-dropdown.js';
+import type { LibroJupyterModel } from '../libro-jupyter-model.js';
 
 import './index.less';
+import { kernelStatus } from '../libro-jupyter-protocol.js';
+import { getServiceStatusInfo } from '../utils/index.js';
 
-export interface ServerStatus {
-  category: string;
-  color: string;
-  text: string;
-  text_zh: string;
-}
-
-export const statusToColor = {
-  canRunImmediate: green[5],
-  canRun: blue[5],
-  blocking: gold[5],
-  error: red[4],
-};
-
-export const jupyterServiceStatus: Record<string, ServerStatus> = {
-  loading: {
-    category: 'JupyterService',
-    color: statusToColor.blocking,
-    text: 'loading',
-    text_zh: l10n.t('加载中'),
-  },
-  failed: {
-    category: 'JupyterService',
-    color: statusToColor.error,
-    text: 'failed',
-    text_zh: l10n.t('加载失败'),
-  },
-  loaded: {
-    category: 'JupyterService',
-    color: statusToColor.canRunImmediate,
-    text: 'loaded',
-    text_zh: l10n.t('加载完成'),
-  },
-};
-
-export const kernelStatus: Record<string, ServerStatus> = {
-  connecting: {
-    category: 'Kernel',
-    color: statusToColor.blocking,
-    text: 'connecting',
-    text_zh: l10n.t('正在连接'),
-  },
-  unknown: {
-    category: 'Kernel',
-    color: statusToColor.blocking,
-    text: 'unknown',
-    text_zh: l10n.t('未知'),
-  },
-  starting: {
-    category: 'Kernel',
-    color: statusToColor.blocking,
-    text: 'starting',
-    text_zh: l10n.t('启动中'),
-  },
-  idle: {
-    category: 'Kernel',
-    color: statusToColor.canRunImmediate,
-    text: 'idle',
-    text_zh: l10n.t('空闲'),
-  },
-  busy: {
-    category: 'Kernel',
-    color: statusToColor.canRun,
-    text: 'busy',
-    text_zh: l10n.t('忙碌'),
-  },
-  terminating: {
-    category: 'Kernel',
-    color: statusToColor.blocking,
-    text: 'terminating',
-    text_zh: l10n.t('终止中'),
-  },
-  restarting: {
-    category: 'Kernel',
-    color: statusToColor.blocking,
-    text: 'restarting',
-    text_zh: l10n.t('重启中'),
-  },
-  autorestarting: {
-    category: 'Kernel',
-    color: statusToColor.blocking,
-    text: 'autorestarting',
-    text_zh: l10n.t('自动重启中'),
-  },
-  dead: {
-    category: 'Kernel',
-    color: statusToColor.error,
-    text: 'dead',
-    text_zh: l10n.t('死亡'),
-  },
-};
-
-const getServiceStatusInfo = (
-  serverManager: ServerManager | undefined,
-  libroModel: LibroJupyterModel | undefined,
-): ServerStatus => {
-  if (!serverManager || serverManager.launching) {
-    return jupyterServiceStatus['loading'];
-  }
-
-  if (
-    !libroModel ||
-    !(libroModel instanceof LibroJupyterModel) ||
-    libroModel.kernelConnecting === true ||
-    libroModel.kernelConnecting === undefined
-  ) {
-    return kernelStatus['connecting'];
-  }
-
-  if (!libroModel.kernelConnection) {
-    return {
-      color: statusToColor.blocking,
-      text: 'no kernel',
-      category: 'Kernel',
-      text_zh: l10n.t('无内核'),
-    };
-  }
-
-  return kernelStatus[libroModel.kernelConnection.status];
-};
+import { KernelSelector } from './kernel-selector-dropdown.js';
 
 export const KernelStatusSelector: React.FC = () => {
   const libroView = useInject<LibroView>(ViewInstance);
