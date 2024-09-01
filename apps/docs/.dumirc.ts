4ded829f0c1153bf8545401ccfacfe4147073cff
sunshinesmilelk <1176136681@qq.com>
2024-09-02 02:53:23 +0800

    feat: add intergration doc page

--- apps/docs/.dumirc.ts
+++ apps/docs/.dumirc.ts
@@ -10,8 +10,8 @@ export default defineConfig({
       { title: '介绍', link: '/introduction' },
       { title: '快速开始', link: '/quickstart' },
       { title: '使用文档', link: '/manual' },
-      { title: '文档', link: '/docs' },
-      { title: '示例', link: '/examples' },
+      { title: '集成文档', link: '/integration' },
+      // { title: '示例', link: '/examples' },
     ],
     banner: {
       title: 'libro',
