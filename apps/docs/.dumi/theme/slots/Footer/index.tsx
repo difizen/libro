7adc66f449886cd0ee354a2d8807bc096dd736e9
sunshinesmilelk <1176136681@qq.com>
2024-09-03 09:03:50 +0800

    fix: mobile adaptation for web pages

--- apps/docs/.dumi/theme/slots/Footer/index.tsx
+++ apps/docs/.dumi/theme/slots/Footer/index.tsx
@@ -17,38 +17,37 @@ const Footer: React.FC = () => {
   return (
     <div className="difizen-dumi-footer">
       <div className="difizen-dumi-footer-content">
-        <div className="difizen-dumi-footer-logo">
-          {themeConfig.logo && (
-            <Link to={themeConfig['link']}>
-              <img className="difizen-dumi-header-logo-img" src={themeConfig.logo} />
-              <span>{themeConfig.name}</span>
-            </Link>
-          )}
-        </div>
-        {(themeConfig['links'] || []).map((datum: DatumType) => (
-          <div className="difizen-dumi-footer-text-group" key={datum.title}>
-            <div className="difizen-dumi-footer-title">{datum.title}</div>
-            {datum.itemList.map((item) => (
-              <div className="difizen-dumi-footer-item" key={item.name}>
-                <a href={item.link} target="_blank" rel="noreferrer">
-                  {item.name}
-                </a>
-              </div>
-            ))}
-          </div>
-        ))}
-        <div className="difizen-dumi-footer-text-group">
-          <div className="difizen-dumi-footer-title">联系我们</div>
-          {qrcodes.map((item: { name: string; qrcode: string }) => (
-            <div className="difizen-dumi-footer-image-group" key={item.name}>
-              <img className="difizen-dumi-footer-img" src={item.qrcode} />
-              <label>{item.name}</label>
+            <div className="difizen-dumi-footer-logo">
+              {themeConfig.logo && (
+                <Link to={themeConfig['link']}>
+                  <img className="difizen-dumi-header-logo-img" src={themeConfig.logo} />
+                  <span>{themeConfig.name}</span>
+                </Link>
+              )}
             </div>
-          ))}
-        </div>
+            {(themeConfig['links'] || []).map((datum: DatumType) => (
+                  <div className="difizen-dumi-footer-text-group" key={datum.title}>
+                    <div className="difizen-dumi-footer-title">{datum.title}</div>
+                    {datum.itemList.map((item) => (
+                      <div className="difizen-dumi-footer-item" key={item.name}>
+                        <a href={item.link} target="_blank" rel="noreferrer">
+                          {item.name}
+                        </a>
+                      </div>
+                    ))}
+                  </div>
+            ))}
+            <div className="difizen-dumi-footer-text-group">
+              <div className="difizen-dumi-footer-contact">联系我们</div>
+              {qrcodes.map((item: { name: string; qrcode: string }) => (
+                <div className="difizen-dumi-footer-image-group" key={item.name}>
+                  <img className="difizen-dumi-footer-img" src={item.qrcode} />
+                  <label>{item.name}</label>
+                </div>
+              ))}
+            </div>
       </div>
-
-      <div className="difizen-dumi-footer-extra">{themeConfig.footer}</div>
+          <div className="difizen-dumi-footer-extra">{themeConfig.footer}</div>
     </div>
   );
 };
