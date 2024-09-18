f40a201fc8eaa27e2e23c1b137a2de29b68ae80b
sunshinesmilelk <1176136681@qq.com>
2024-09-18 23:57:12 +0800

    refactor: use cofine editor as default editor

--- packages/libro-cofine-editor/src/editor-contribution.ts
+++ packages/libro-cofine-editor/src/editor-contribution.ts
@@ -3,7 +3,6 @@ import {
   CodeEditorContribution,
   LanguageSpecRegistry,
 } from '@difizen/libro-code-editor';
-import { MIME } from '@difizen/libro-common';
 import { inject, singleton } from '@difizen/mana-app';
 
 import {
@@ -34,11 +33,7 @@ export class LibroE2EditorContribution implements CodeEditorContribution {
     });
   };
 
-  canHandle(mime: string): number {
-    const mimes = [MIME.odpssql, MIME.python, MIME.prompt] as string[];
-    if (mimes.includes(mime)) {
-      return 50 + 1;
-    }
-    return 0;
+  canHandle(): number {
+    return 50;
   }
 }
