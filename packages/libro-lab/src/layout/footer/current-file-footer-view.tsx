1c3623ac68a24000060469c7b1c6370183e4475a
sunshinesmilelk <1176136681@qq.com>
2024-10-02 00:15:53 +0800

    fix: fix some bilingual text error

--- packages/libro-lab/src/layout/footer/current-file-footer-view.tsx
+++ packages/libro-lab/src/layout/footer/current-file-footer-view.tsx
@@ -18,12 +18,15 @@ const CurrentFileFooterComponent = React.forwardRef(function CurrentFileFooterCo
   ref: React.ForwardedRef<HTMLDivElement>,
 ) {
   const currentFileFooterView = useInject<LibroLabCurrentFileFooterView>(ViewInstance);
-
+  const label = currentFileFooterView.navigatableView?.title.label;
   return (
     <div className="libro-lab-current-file-footer" ref={ref}>
-      <span>{`${l10n.t('当前文件：')}${
-        currentFileFooterView.navigatableView?.title.label || ''
-      }`}</span>
+      <span>{l10n.t('当前文件：')}</span>
+      {
+        typeof label === 'function'
+          ? React.createElement(label) // 如果是 React.FC，调用它
+          : label /* 如果是 ReactNode，直接渲染 */
+      }
     </div>
   );
 });
