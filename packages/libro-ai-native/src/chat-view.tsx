123a1e05962076aff32b824c347917fee18c35a0
sunshinesmilelk <1176136681@qq.com>
2024-10-24 18:11:11 +0800

    feat(ai-native): add ai chat

--- packages/libro-ai-native/src/chat-view.tsx
+++ packages/libro-ai-native/src/chat-view.tsx
@@ -1,25 +1,55 @@
 import type { DisplayView, LibroView } from '@difizen/libro-jupyter';
+import { ChatView } from '@difizen/magent-chat';
 import type { ConfigurationService } from '@difizen/mana-app';
-import { BaseView, prop, transient, view } from '@difizen/mana-app';
+import {
+  BaseView,
+  prop,
+  transient,
+  view,
+  inject,
+  ViewManager,
+  useInject,
+  ViewInstance,
+  ViewRender,
+} from '@difizen/mana-app';
 import { useRef } from 'react';
 
 export const ChatRender = () => {
   const containRef = useRef<HTMLDivElement>(null);
+  const libroChatView = useInject<LibroChatView>(ViewInstance);
   return (
     <div className="chat-container" ref={containRef}>
-      chat
+      <ViewRender view={libroChatView.chatView}></ViewRender>
     </div>
   );
 };
 
 @transient()
 @view('libro-chat-view')
-export class ChatView extends BaseView implements DisplayView {
+export class LibroChatView extends BaseView implements DisplayView {
   parent: LibroView | undefined = undefined;
   protected configurationService: ConfigurationService;
 
   override view = ChatRender;
 
+  chatView: ChatView;
+
   @prop()
   isDisplay = true;
+
+  constructor(@inject(ViewManager) viewManager: ViewManager) {
+    super();
+    viewManager
+      .getOrCreateView(ChatView, {
+        id: this.id,
+        chat_key: 'LLM:chatgpt',
+      })
+      .then((chatView) => {
+        this.chatView = chatView;
+        return;
+      })
+      .catch(() => {
+        //
+      });
+  }
 }
--- packages/libro-ai-native/src/module.ts
+++ packages/libro-ai-native/src/module.ts
@@ -1,19 +1,22 @@
 import { ErrorOutputModel } from '@difizen/libro-jupyter';
+import { LibroChatModule } from '@difizen/magent-libro';
 import { ManaModule } from '@difizen/mana-app';
 
 import { LibroAINativeCommandContribution } from './ai-native-command-contribution.js';
 import { LibroAIChatSlotContribution } from './chat-slot-contribution.js';
-import { ChatView } from './chat-view.js';
+import { LibroChatView } from './chat-view.js';
 import { AIErrorOutputModel } from './error-output-model.js';
 import { LibroAINativeColorRegistry } from './libro-ai-native-color-registry.js';
 
-export const LibroAINativeModule = ManaModule.create().register(
-  LibroAINativeColorRegistry,
-  ChatView,
-  LibroAIChatSlotContribution,
-  LibroAINativeCommandContribution,
-  {
-    token: ErrorOutputModel,
-    useClass: AIErrorOutputModel,
-  },
-);
+export const LibroAINativeModule = ManaModule.create()
+  .register(
+    LibroAINativeColorRegistry,
+    LibroChatView,
+    LibroAIChatSlotContribution,
+    LibroAINativeCommandContribution,
+    {
+      token: ErrorOutputModel,
+      useClass: AIErrorOutputModel,
+    },
+  )
+  .dependOn(LibroChatModule);
