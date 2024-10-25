807fe53b55d1bf4668ae39b872c5c4f000c6f12e
sunshinesmilelk <ximo.lk@antgroup.com>
2024-10-25 15:00:19 +0800

    feat(ai-native): add style when cell chat active

--- packages/libro-ai-native/src/libro-ai-native-color-registry.ts
+++ packages/libro-ai-native/src/libro-ai-native-color-registry.ts
@@ -1,4 +1,5 @@
 import type { ColorRegistry } from '@difizen/mana-app';
+import { Color } from '@difizen/mana-app';
 import { singleton, ColorContribution } from '@difizen/mana-app';
 
 @singleton({ contrib: ColorContribution })
@@ -7,8 +8,19 @@ export class LibroAINativeColorRegistry implements ColorContribution {
     colors.register(
       // #region antd variable
       {
-        id: 'libro.ai.native.border',
+        id: 'libro.ai.native.color',
         defaults: { dark: '#bd74e8', light: '#bd74e8' },
+        description: '',
+      },
+    );
+    colors.register(
+      // #region antd variable
+      {
+        id: 'libro.ai.native.box.shadow',
+        defaults: {
+          dark: Color.rgba(203, 146, 197, 0.37),
+          light: Color.rgba(203, 146, 197, 0.37),
+        },
         description: '',
       },
     );
