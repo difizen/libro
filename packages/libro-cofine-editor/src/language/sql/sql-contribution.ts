1eabd57bc9f95081641b661b54faf885bd3cd11d
brokun <yukun.wyk@antgroup.com>
2024-10-30 02:14:41 +0800

    feat(editor): #268 sql editor highlight

---
+++ packages/libro-cofine-editor/src/language/sql/sql-contribution.ts
@@ -0,0 +1,21 @@
+import { InitializeContribution } from '@difizen/libro-cofine-editor-core';
+import { singleton } from '@difizen/mana-app';
+import * as monaco from '@difizen/monaco-editor-core';
+
+import { id } from './config.js';
+import { setTokensLanguage } from './theme-provider.js';
+
+@singleton({ contrib: [InitializeContribution] })
+export class SqlContribution implements InitializeContribution {
+  onInitialize() {
+    monaco.languages.register({
+      id,
+      extensions: ['.sql'],
+      aliases: ['sql', 'odps'],
+      mimetypes: ['text/sql'],
+    });
+    monaco.languages.onLanguage(id, () => {
+      setTokensLanguage();
+    });
+  }
+}
