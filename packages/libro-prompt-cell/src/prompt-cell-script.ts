f8021da0e8811e2cba0229f007175eb50f2087ca
sunshinesmilelk <ximo.lk@antgroup.com>
2024-11-01 16:30:42 +0800

    fix(prompt): update prompt cell script

--- packages/libro-prompt-cell/src/prompt-cell-script.ts
+++ packages/libro-prompt-cell/src/prompt-cell-script.ts
@@ -3,7 +3,7 @@ import { singleton } from '@difizen/mana-app';
 @singleton()
 export class PromptScript {
   public readonly getChatObjects: string = `from libro_ai import chat_object_manager
-chat_object_manager.dump_list_json()`;
+chat_object_manager.dump_kernel_list_json()`;
   public readonly getChatRecoreds: string = `from libro_ai import chat_record_provider
 chat_record_provider.get_records()`;
 }
