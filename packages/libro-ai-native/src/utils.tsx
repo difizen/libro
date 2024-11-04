4c0e7aea18d568f0124b82ff8b65c1beb16b2566
sunshinesmilelk <ximo.lk@antgroup.com>
2024-11-04 12:57:21 +0800

    fix(ai-native): fix cursor in chat

--- packages/libro-ai-native/src/utils.tsx
+++ packages/libro-ai-native/src/utils.tsx
@@ -1,7 +1,4 @@
 import type { CellView } from '@difizen/libro-jupyter';
-import { Modal } from 'antd';
-import { useEffect, useState } from 'react';
-import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch';
 
 export function stringToReadableStream(inputString: string) {
   // Convert the string into a Uint8Array
@@ -33,83 +30,4 @@ export function cancelCellAIClassname(cell: CellView) {
   if (cell.className?.includes(' ai-cell-focus')) {
     cell.className = cell.className?.replace(' ai-cell-focus', '');
   }
-}
-
-export function ImageModal({ src, alt }: any) {
-  const [visible, setVisible] = useState(false);
-  const [imageDimensions, setImageDimensions] = useState({
-    width: 0,
-    height: 0,
-  });
-
-  useEffect(() => {
-    const img = new Image();
-    img.src = src;
-    img.onload = () => {
-      setImageDimensions({
-        width: img.width,
-        height: img.height,
-      });
-    };
-  }, [src]);
-
-  const maxModalWidth = window.innerWidth * 0.8; // 80% of the viewport width
-  const maxModalHeight = window.innerHeight * 0.8; // 80% of the viewport height
-
-  let adjustedWidth, adjustedHeight;
-
-  const aspectRatio = imageDimensions.width / imageDimensions.height;
-
-  if (imageDimensions.width > maxModalWidth) {
-    adjustedWidth = maxModalWidth;
-    adjustedHeight = adjustedWidth / aspectRatio;
-  } else if (imageDimensions.height > maxModalHeight) {
-    adjustedHeight = maxModalHeight;
-    adjustedWidth = adjustedHeight * aspectRatio;
-  } else {
-    adjustedWidth = imageDimensions.width;
-    adjustedHeight = imageDimensions.height;
-  }
-
-  return (
-    <div>
-      <img
-        src={src}
-        alt={alt}
-        style={{ cursor: 'pointer' }}
-        onClick={() => setVisible(true)}
-        onLoad={() => {
-          // 解决生成图片没有滚动到最下方的问题。
-          document.getElementById('chat-main-scroll')?.scrollIntoView(false);
-        }}
-      />
-      <Modal
-        open={visible}
-        closeIcon={false}
-        footer={null}
-        width={adjustedWidth + 30}
-        onCancel={() => setVisible(false)}
-        bodyStyle={{
-          padding: 0,
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: adjustedHeight,
-        }}
-      >
-        <TransformWrapper initialScale={1} initialPositionX={0} initialPositionY={0}>
-          <TransformComponent>
-            <img
-              src={src}
-              alt={alt}
-              style={{
-                width: '100%',
-                height: '100%',
-              }}
-            />
-          </TransformComponent>
-        </TransformWrapper>
-      </Modal>
-    </div>
-  );
 }
