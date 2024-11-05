0f205a3ab1e3a57559b13555377a87b998d6e052
brokun <yukun.wyk@antgroup.com>
2024-11-05 22:04:40 +0800

    docs: localization

--- apps/docs/.dumi/theme/componets/banner/index.tsx
+++ apps/docs/.dumi/theme/componets/banner/index.tsx
@@ -1,28 +1,82 @@
-import { useSiteData } from 'dumi';
+import { l10n } from '@difizen/mana-l10n';
 import React from 'react';
 
-import { firstfeatures, secondfeatures } from './constants';
+import type { FeatureProps} from './constants';
 import { FeatureSection } from './feature-section';
 import { HeroSection } from './hero-section';
 import './index.less';
 import { IntergractionSection } from './intergration';
 
 const Banner: React.FC = () => {
-  const { themeConfig } = useSiteData();
 
-  if (!themeConfig.banner) {
-    return null;
-  }
+
+  const firstfeatures: FeatureProps[] = [
+    {
+      title: l10n.t('Multi-node debugging in secret computing scenarios.'),//'隐私计算场景多节点调试',
+      description:
+        l10n.t(`SecretNote is an advanced tool suite designed specifically for Enigma developers. It supports multi-node code execution and file management, while also providing runtime status tracking features, significantly enhancing developer's efficiency and work experience.`),
+      imageUrl:
+        '/scretenote.png',
+      isImageLeft: true,
+    },
+    {
+      title: l10n.t('Intelligent code generation for quantitative scenarios.'),
+      description:l10n.t('In the coding environment most comfortable for quantitative analysts, seamlessly integrate large model ecosystems and custom agents through Prompt Cell.'),
+      imageUrl:
+        '/zhu.png',
+      isImageLeft: false,
+    },
+    {
+      title: l10n.t('Big Data SQL Interaction Enhancement'),
+      description: l10n.t('Provide powerful kernel customization capabilities, supporting execution environments like ODPS SQL.'),
+      imageUrl:
+        '/sql_ide.png',
+      isImageLeft: true,
+    },
+  ];
+
+
+  const secondfeatures: FeatureProps[] = [
+    {
+      title: 'AI Copilot',
+      description: l10n.t('Support intelligent assistant AI dialogue functions.'),
+      imageUrl:
+        '/copilot.png',
+      isImageLeft: true,
+    },
+    {
+      title: l10n.t('Notebook as an App'),
+      description: l10n.t('Generate dynamic reports based on Notebooks combined with interactive controls.'),
+      imageUrl:
+        '/app.png',
+      isImageLeft: false,
+    },
+    {
+      title: l10n.t('Version Diff Capability'),
+      description: l10n.t('Support cell-level version diff capability for better version management and code review.'),
+      imageUrl:
+        '/diff.png',
+      isImageLeft: true,
+    },
+    {
+      title: l10n.t('Superior code suggestion capabilities'),
+      description:
+      l10n.t('Libro offers an exceptional editing experience, especially for Python code, providing superior code completion, code suggestions, code formatting, and go-to-definition features.'),
+      imageUrl:
+        '/tip.png',
+      isImageLeft: false,
+    },
+  ];
   return (
     <div className="difizen-dumi-banner">
       <HeroSection></HeroSection>
       <FeatureSection
         features={firstfeatures}
-        title="企业级场景定制能力"
+        title={l10n.t('Enterprise-level scenario customization capabilities')}
       ></FeatureSection>
       <FeatureSection
         features={secondfeatures}
-        title="激动人心的功能特性"
+        title={l10n.t('Exciting features and capabilities')}
       ></FeatureSection>
       <IntergractionSection></IntergractionSection>
     </div>
