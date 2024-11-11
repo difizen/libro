dd533fd1e96816bd3099910ad24ab4091a483d99
sunshinesmilelk <ximo.lk@antgroup.com>
2024-11-11 13:46:16 +0800

    feat(lab): configurable left-side panel supported

--- packages/libro-lab/src/toc/module.ts
+++ packages/libro-lab/src/toc/module.ts
@@ -1,21 +1,8 @@
 import { LibroTOCModule } from '@difizen/libro-toc';
-import { createViewPreference, ManaModule } from '@difizen/mana-app';
-
-import { LibroLabLayoutSlots } from '../layout/protocol.js';
+import { ManaModule } from '@difizen/mana-app';
 
 import { TocPanelView } from './libro-toc-panel-view.js';
 
 export const LibroLabTocModule = ManaModule.create()
-  .register(
-    TocPanelView,
-    createViewPreference({
-      view: TocPanelView,
-      slot: LibroLabLayoutSlots.navigator,
-      autoCreate: true,
-      openOptions: {
-        reveal: true,
-        order: 'toc',
-      },
-    }),
-  )
+  .register(TocPanelView)
   .dependOn(LibroTOCModule);
