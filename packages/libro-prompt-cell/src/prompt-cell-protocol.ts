49a351e6e73dc785ef824ba04339a17ae178ba90
sunshinesmilelk <ximo.lk@antgroup.com>
2024-11-20 15:01:56 +0800

    feat(prompt): support interpreter code mimetype

--- packages/libro-prompt-cell/src/prompt-cell-protocol.ts
+++ packages/libro-prompt-cell/src/prompt-cell-protocol.ts
@@ -1,3 +1,4 @@
+import type { PartialJSONObject } from '@difizen/libro-common';
 import type { CellOptions } from '@difizen/libro-core';
 
 import type { LibroPromptCellModel } from './prompt-cell-model.js';
@@ -6,3 +7,8 @@ export type LibroPromptCellModelFactory = (
   options: CellOptions,
 ) => LibroPromptCellModel;
 export const LibroPromptCellModelFactory = Symbol('LibroPromptCellModelFactory');
+
+export interface InterpreterMeta extends PartialJSONObject {
+  interpreter_code: string;
+  interpreter_text: string;
+}
