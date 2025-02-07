881b37b6a15c851953b781453fd35747d867b188
sunshinesmilelk <1176136681@qq.com>
2025-02-07 22:40:56 +0800

    chore: delete babel-preset-mana

--- mana/packages/babel-preset-mana/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  root: true,
-  extends: [require.resolve('../../.eslintrc.js')],
-  rules: {
-    '@typescript-eslint/no-namespace': 'off',
-    '@typescript-eslint/no-redeclare': 'off',
-  },
-};
